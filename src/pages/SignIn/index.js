7844e31803c134781ba30d3da8009f2e3b51353e
Lucas Siqueira <lucassiqzro@gmail.com>
2020-01-25 15:54:21 -0300

    Create input component

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
 
+import Input from '~/components/Input';
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Background />;
+  return (
+    <Background>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+    </Background>
+  );
 }
